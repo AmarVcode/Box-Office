efd7074ae37204546dc64121f57e63c4b7848801
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-02-16 00:21:05 +0530

    amazing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
+
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      
+      <Navbar />
+      <Home />
     </div>
   )
 }
---
+++ my-app/src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './styles/Home.scss'
+const Home = () => {
+  return (
+    <div className='main'>Home</div>
+  )
+}
+
+export default Home
